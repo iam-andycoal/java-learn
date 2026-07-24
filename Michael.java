9b156bcbd95ffac8c5d2dee4dde7beed7c60a66e
noel knowles <coalzcoley@gmail.com>
2026-07-24 16:48:38 +0300

    Add files via upload

---
+++ Michael.java
@@ -0,0 +1,9 @@
+public class Michael 
+{
+  public static void main (String [] args)
+  {
+    System.out.println("Hello, my name is Michael.");
+    System.out.println("I am a Java developer.");
+    System.out.println("I enjoy programming and learning new technologies.");
+  }
+}
